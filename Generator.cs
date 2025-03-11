fcc37d95b6d2ce78a754839415d01ae719da2c74
bobfishkins23 <bobfishkins23@yahoo.com>
2025-03-10 23:50:57 -0400

    add link to porting instructions

--- Generator.cs
+++ Generator.cs
@@ -25,7 +25,9 @@ namespace StructureHelper
 		/// <param name="ignoreNull">If the structure should repsect the normal behavior of null tiles or not. This should never be true if you're using the mod as a dll reference.</param>
 		/// <param name="flags">Allows you to pass flags for special generation behavior. See <see cref="GenFlags"/> </param>
 		/// <returns>If the structure generated successfully or not</returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static bool GenerateStructure(string path, Point16 pos, Mod mod, bool fullPath = false, bool ignoreNull = false, GenFlags flags = GenFlags.None)
 		{
 			return API.Legacy.LegacyGenerator.GenerateStructure(path, pos, mod, fullPath, ignoreNull, flags);
@@ -41,7 +43,9 @@ namespace StructureHelper
 		/// <param name="ignoreNull">If the structure should repsect the normal behavior of null tiles or not. This should never be true if you're using the mod as a dll refference.</param>
 		/// <param name="flags">Allows you to pass flags for special generation behavior. See <see cref="GenFlags"/> </param>
 		/// <returns>If the structure generated successfully or not</returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static bool GenerateMultistructureRandom(string path, Point16 pos, Mod mod, bool fullPath = false, bool ignoreNull = false, GenFlags flags = GenFlags.None)
 		{
 			return API.Legacy.LegacyGenerator.GenerateMultistructureRandom(path, pos, mod, fullPath, ignoreNull, flags);
@@ -58,7 +62,9 @@ namespace StructureHelper
 		/// <param name="ignoreNull">If the structure should repsect the normal behavior of null tiles or not. This should never be true if you're using the mod as a dll refference.</param>
 		/// <param name="flags">Allows you to pass flags for special generation behavior. See <see cref="GenFlags"/> </param>
 		/// <returns>If the structure generated successfully or not</returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static bool GenerateMultistructureSpecific(string path, Point16 pos, Mod mod, int index, bool fullPath = false, bool ignoreNull = false, GenFlags flags = GenFlags.None)
 		{
 			return API.Legacy.LegacyGenerator.GenerateMultistructureSpecific(path, pos, mod, index, fullPath, ignoreNull, flags);
@@ -72,7 +78,9 @@ namespace StructureHelper
 		/// <param name="dims">The Point16 variable which you want to be set to the dimensions of the structure.</param>
 		/// <param name="fullPath">Indicates if you want to use a fully qualified path to get the structure file instead of one from your mod - generally should only be used for debugging.</param>
 		/// <returns></returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static bool GetDimensions(string path, Mod mod, ref Point16 dims, bool fullPath = false)
 		{
 			return API.Legacy.LegacyGenerator.GetDimensions(path, mod, ref dims, fullPath);
@@ -87,7 +95,9 @@ namespace StructureHelper
 		/// <param name="dims">The Point16 variable which you want to be set to the dimensions of the structure.</param>
 		/// <param name="fullPath">Indicates if you want to use a fully qualified path to get the structure file instead of one from your mod - generally should only be used for debugging.</param>
 		/// <returns></returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static bool GetMultistructureDimensions(string path, Mod mod, int index, ref Point16 dims, bool fullPath = false)
 		{
 			return API.Legacy.LegacyGenerator.GetMultistructureDimensions(path, mod, index, ref dims, fullPath);
@@ -99,7 +109,9 @@ namespace StructureHelper
 		/// <param name="path">The path to the structure file you wish to check.</param>
 		/// <param name="mod">The instance of your mod to grab the file from.</param>
 		/// <returns>True if the file is a multistructure, False if the file is a structure, null if it is invalid.</returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static bool? IsMultistructure(string path, Mod mod)
 		{
 			return API.Legacy.LegacyGenerator.IsMultistructure(path, mod);
@@ -111,7 +123,9 @@ namespace StructureHelper
 		/// <param name="path">The path to the structure file you wish to check.</param>
 		/// <param name="mod">The instance of your mod to grab the file from.</param>
 		/// <returns>The amount of structures in a multistructure, or null if invalid.</returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static int? GetStructureCount(string path, Mod mod)
 		{
 			return API.Legacy.LegacyGenerator.GetStructureCount(path, mod);
@@ -124,7 +138,9 @@ namespace StructureHelper
 		/// <param name="pos">The position in the world of the top-leftmost tile to be placed at</param>
 		/// <param name="ignoreNull">If this structure should place null tiles or not</param>
 		/// <returns>If the structure successfully generated or not</returns>
-		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator. If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
+		[Obsolete("Legacy generation API is deprecated. Please port your structures to the 3.0 format and use StructureHelper.API.Generator." +
+			"\n Porting instructions can be found at https://github.com/ScalarVector1/StructureHelper/wiki/3.0-Porting-Guide" +
+			"\n If you truly need legacy API access, use StructureHelper.API.Legacy.LegacyGenerator.", true)]
 		public static unsafe bool Generate(TagCompound tag, Point16 pos, bool ignoreNull = false, GenFlags flags = GenFlags.None)
 		{
 			return API.Legacy.LegacyGenerator.Generate(tag, pos, ignoreNull, flags);
