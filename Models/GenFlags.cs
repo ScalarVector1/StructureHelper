54083b3bc44db3bfa5439c02bd5bac3454708522
bobfishkins23 <bobfishkins23@yahoo.com>
2025-03-11 00:27:57 -0400

    Change namespace to keep runtime compat

--- Models/GenFlags.cs
+++ Models/GenFlags.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StructureHelper.Models
+namespace StructureHelper
 {
 	/// <summary>
 	/// Flags that allow users to define special behavior for structure generation.
